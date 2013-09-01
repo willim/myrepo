47aa7b0175f253d0e23755f6fdc74a6dc6cf8efb
willim <371102059@qq.com>
2013-09-01 23:18:44 +0800

    111

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by mac on 13-9-1.
+//  Copyright (c) 2013年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ test/test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by mac on 13-9-1.
+//  Copyright (c) 2013年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
